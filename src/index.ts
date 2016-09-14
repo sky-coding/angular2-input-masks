3e29a9421a993e8323b07fbee1fee59e9e83170f
Skyler Brown <skbrown@LD.corp.local>
2016-09-14 11:35:18 -0700

    export NgModule

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,18 @@ import '@angular/common';
 import '@angular/compiler';
 import '@angular/forms';
 
+import {NgModule} from '@angular/core'
+
+import {MaskingDirective, MaskType} from './components/masking.directive';
 export {MaskingDirective, MaskType} from './components/masking.directive';
+
+@NgModule({
+  declarations: [
+    MaskingDirective
+  ],
+  exports: [
+    MaskingDirective
+  ],
+  providers: []
+})
+export class MaskingModule {}
